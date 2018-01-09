cd2160f1dbd1d9049769a0d163768b54fd3f05d0
Andrew Zenk <azenk@umn.edu>
2018-01-08 21:47:32 -0600

    Add support for ca_path configuration parameter
    If working with multiple CAs, we need support for the ca_path parameter
    which allows consul to use a directory of CA certificates rather than a
    single file.

--- libraries/consul_config.rb
+++ libraries/consul_config.rb
@@ -58,6 +58,7 @@ module ConsulCookbook
       attribute(:bootstrap, equal_to: [true, false])
       attribute(:bootstrap_expect, kind_of: Integer)
       attribute(:ca_file, kind_of: String)
+      attribute(:ca_path, kind_of: String)
       attribute(:cert_file, kind_of: String)
       attribute(:check_update_interval, kind_of: String)
       attribute(:client_addr, kind_of: String)
@@ -222,7 +223,7 @@ module ConsulCookbook
         )
 
         for_keeps << %i(bootstrap bootstrap_expect) if server
-        for_keeps << %i(ca_file cert_file key_file) if tls?
+        for_keeps << %i(ca_file ca_path cert_file key_file) if tls?
         for_keeps = for_keeps.flatten
 
         # Filter out undefined attributes and keep only those listed above
